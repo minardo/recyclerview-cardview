dd8f29ffb41fe40d125e47c2a8355e90d810f229
Muhamad Minardo <ardo15.project@gmail.com>
2018-06-20 21:26:20 +0700

    Implement Adapter and Item in RecyclerView MainActivity

--- app/src/main/java/com/example/ardo/recyclerviewcardview/MainActivity.java
+++ app/src/main/java/com/example/ardo/recyclerviewcardview/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.ardo.recyclerviewcardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView lstChatHistory;
+    LinearLayoutManager linear;
+
+    List<PolaItem> item;
+    ChatAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstChatHistory = (RecyclerView) findViewById(R.id.lst_chat_history);
+
+        linear = new LinearLayoutManager(this);
+        lstChatHistory.setLayoutManager(linear);
+
+        item = new ArrayList<>();
+        item.add(new PolaItem(0, "Yuningsih","Assalamualaikum", "07:32 PM"));
+        item.add(new PolaItem(0, "Monic","Mas ardo lagi dimana?", "10:54 AM"));
+        item.add(new PolaItem(0, "Ardo Manis","Hallo ardo", "09:31 AM"));
+
+        adapter = new ChatAdapter(this, item);
+
+        lstChatHistory.setAdapter(adapter);
     }
 }
